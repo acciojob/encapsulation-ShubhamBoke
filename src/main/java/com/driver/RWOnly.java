aae867e295b500540bba744d4d8435dc9b7ce6b9
Shubham Boke <shubhamboke.99@gmail.com>
2023-06-08 10:59:38 +0530

    r Please enter the commit message for your changes. Lines starting r	new filer   .idea/encodings.xml r	new file:   .idea/jarRepositories.xml r	new file:   .idea/misc.xml

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void setName(String s){
+        this.name = s;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
